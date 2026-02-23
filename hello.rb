c292e8b79ac5366d5a82dd1736db3bf152020cd4
Timalsinarabin <timalsinarabin11@gmail.com>
2026-02-23 07:15:33 +0000

    feat: added registation

--- hello.rb
+++ hello.rb
@@ -1,6 +1,19 @@
 require "sinatra"
-
-get '/' do
-  
+require "sqlite3"
+get ['/','/login'] do
+  db = SQLite3::Database.new 'auth.db'
+  db.execute <<-SQL
+  CREATE TABLE IF NOT EXISTS users(
+    id INTEGER PRIMARY KEY,
+    username VARCHAR(50),
+    password VARCHAR(50)
+  )
+  SQL
   erb :index;
+end
+get '/register' do
+  erb :register;
+end
+post '/login' do
+  "Hello user"
 end
