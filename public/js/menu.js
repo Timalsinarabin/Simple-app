dfdca2123f126728557e0828881b1dca853a2780
Timalsinarabin <timalsinarabin11@gmail.com>
2026-02-27 11:19:26 +0000

    feat: add pagination

--- public/js/menu.js
+++ public/js/menu.js
@@ -4,12 +4,26 @@ const viewAllBooks = document.querySelector('.viewall-books');
 const addbookform = document.querySelector('.add-book-form');
 const removebook = document.querySelector('.nav-removebook');
 const removebookform = document.querySelector('.remove-book-form');
+const menu = document.querySelector('.side-menu');
+const title = document.querySelector('.title');
+const logout = document.querySelector('.nav-logout');
+
+const sections = [removebook, viewall, addbook, title, logout]
+
+console.log("Menu JS loaded");
+
+menu.addEventListener('click', () => {
+    sections.forEach(section => {
+        section.classList.toggle('hidden');
+    });
+});
 
 addbook.addEventListener('click', () => {
     addbookform.classList.remove('hidden');
     removebookform.classList.add('hidden');
     viewAllBooks.classList.add('hidden');
 
+    removebook.classList.remove('bg-purple-200');
     viewall.classList.remove('bg-purple-200');
     addbook.classList.add('bg-purple-200');
 });
@@ -19,11 +33,13 @@ viewall.addEventListener('click', () => {
     removebookform.classList.add('hidden');
     viewAllBooks.classList.remove('hidden');
 
+    removebook.classList.remove('bg-purple-200');
     addbook.classList.remove('bg-purple-200');
     viewall.classList.add('bg-purple-200');
 });
 
 removebook.addEventListener('click', () => {
+    addbookform.classList.add('hidden');
     removebookform.classList.remove('hidden');
     viewAllBooks.classList.add('hidden');
 
